11a65386a6e866e75ac1b91ef047b9c2ef647e51
Quentin Pongratz <qpongratz@gmail.com>
2022-04-17 15:52:04 -0500

    Add number validations

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,3 +1,5 @@
 class Kitten < ApplicationRecord
   validates :name, :age, :cuteness, :softness, presence: true
+  validates :cuteness, :softness, numericality: { in: 1..10 }
+  validates :age, numericality: { only_integer: true }
 end
