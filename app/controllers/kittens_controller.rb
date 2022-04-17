a6080bfeed4ff22cb8c68f0858e452c514107189
Quentin Pongratz <qpongratz@gmail.com>
2022-04-17 16:03:34 -0500

    Setup show for json

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,6 +1,11 @@
 class KittensController < ApplicationController
   def show
     @kitten = Kitten.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
   end
 
   def index
