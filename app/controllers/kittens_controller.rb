41718ff068082b415d1c98bb50e7386d0c65e107
Quentin Pongratz <qpongratz@gmail.com>
2022-04-17 13:11:59 -0500

    Setup kittens controller with routes

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,52 @@
+class KittensController < ApplicationController
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new(kitten_params)
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to root_path
+      flash[:notice] = 'You have successfully created a kitten'
+    else
+      render :new, status: :unprocessable_entity
+      flash.now[:alert] = 'This kitten did not save'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.delete
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+
+    if @kitten.update(kitten_params)
+      redirect_to @kitten
+      flash[:notice] = 'Kitten successfully updated'
+    else
+      flash.new[:alert] = 'Kitten did not update successfully'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :softness, :cuteness)
+  end
+end
